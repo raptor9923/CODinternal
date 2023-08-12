8c34bd1a1b1c65f60ade686c7cbf05484c649dd0
Kuba <warzywoda.jakub@gmail.com>
2023-08-12 19:07:31 +0200

    updated

--- CODinternal/getd3d9device.cpp
+++ CODinternal/getd3d9device.cpp
@@ -1,4 +1,24 @@
 #include"getd3d9device.h"
 
-
-GetD3D9Device(void**, size_t size);
+bool GetD3D9Device(void** vTable, size_t size)
+{
+	if (!vTable)
+		return false;
+	IDirect3D9* direct3D9 = Direct3DCreate9(D3D_SDK_VERSION);
+	if (!direct3D9)
+		return false;
+	D3DPRESENT_PARAMETERS d3d9Params;
+	d3d9Params.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3d9Params.hDeviceWindow = GetForegroundWindow();
+	d3d9Params.Windowed = false;
+	IDirect3DDevice9* device;
+	if (FAILED(direct3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, d3d9Params.hDeviceWindow, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3d9Params, &device)))
+	{
+		direct3D9->Release();
+		return false;
+	}
+	memcpy(vTable, *reinterpret_cast<void***>(device),size);
+	device->Release();
+	direct3D9->Release();
+	return true;
+}
--- CODinternal/getd3d9device.h
+++ CODinternal/getd3d9device.h
@@ -2,6 +2,7 @@
 #pragma comment(lib, "d3d9.lib")
 #include<d3d9.h>
 
+
 typedef HRESULT(APIENTRY * tEndScene)(LPDIRECT3DDEVICE9 device);
 
-bool GetD3D9Device(void**, size_t size);
+bool GetD3D9Device(void** vTable, size_t size);
